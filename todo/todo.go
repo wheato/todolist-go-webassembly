d9eb54c2ead9d8eb97243fa357d630968251517f
wheato <wheato2010@gmail.com>
2018-12-20 21:40:22 +0800

    feat: Add Input feature

--- todo/todo.go
+++ todo/todo.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"strconv"
+	"bytes"
+	"html/template"
 )
 
 type TodoList struct{
@@ -12,9 +15,18 @@ type TodoList struct{
 type TodoItem struct{
 	Compeleted bool
 	Text string
-	ID int
+	ID int32
 }
 
+const templ = `
+{{range .Items}}
+	{{ if .Compeleted }}
+		<li class='compeleted' data-index='{{.ID}}'>{{.Text}}</li>
+	{{ else }}
+		<li data-index='{{.ID}}'>{{.Text}}</li>
+	{{ end }}
+{{end}}
+`
 var (
 	tl TodoList
 )
@@ -22,55 +34,68 @@ var (
 func main() {
 	c := make(chan struct{}, 0)
 	addBtn := js.Global().Get("document").Call("querySelector", "#js_addTodo")
+	listDom := js.Global().Get("document").Call("querySelector", "#j_todoList")
+
 	addHandlerCb := js.NewCallback(jsAddHandler)
+	toggleHandlerCb := js.NewCallback(jsToggleHandler)
 	addBtn.Call("addEventListener", "click", addHandlerCb)
+	listDom.Call("addEventListener", "click", toggleHandlerCb)
 	<- c
 }
 
 func jsAddHandler(args []js.Value) {
 	inputDom := js.Global().Get("document").Call("querySelector", "#j_textInput")
+	listDom := js.Global().Get("document").Call("querySelector", "#j_todoList")
 	text := inputDom.Get("value").String()
 	if len(text) > 0 {
 		tl.AddTodoItem(text)
 	}
-	fmt.Printf("%d\n", len(tl.Items))
+	html := tl.Render()
+	fmt.Println(html)
+	listDom.Set("innerHTML", html)
 }
 
-func NewTodo(text string, id int) TodoItem {
+func jsToggleHandler(args []js.Value) {
+	el := args[0].Get("target")
+	elNodeName := el.Get("nodeName").String()
+	if elNodeName == "LI" {
+		listDom := js.Global().Get("document").Call("querySelector", "#j_todoList")
+		id, _ := strconv.ParseInt(el.Get("dataset").Get("index").String(), 10, 32)
+		tl.ToggleStatus(int32(id))
+		html := tl.Render()
+		fmt.Println(html)
+		listDom.Set("innerHTML", html)
+	}
+}
+
+func NewTodo(text string, id int32) TodoItem {
 	return TodoItem{ Compeleted: false, Text: text, ID: id }
 }
 
-func SetId(id int, item TodoItem) {
-	item.ID = id
-}
-
 func (tl *TodoList) AddTodoItem(text string) TodoItem{
-	newItem := NewTodo(text, len(tl.Items))
+	newItem := NewTodo(text, int32(len(tl.Items)))
 	tl.Items = append(tl.Items, newItem)
 	return newItem
 }
 
-func (tl TodoList) GetById(id int) TodoItem {
+func (tl TodoList) GetById(id int32) *TodoItem {
 	for _, item := range tl.Items{
 		if item.ID == id {
-			return item
+			return &item
 		}
 	}
-	return TodoItem{}
+	return &TodoItem{}
 }
 
-func (tl TodoList) ToggleStatus(id int) {
+func (tl TodoList) ToggleStatus(id int32) {
 	target := tl.GetById(id)
-	if target.Text != "" {
-		target.Compeleted = !target.Compeleted
-	}
+	target.Compeleted = !target.Compeleted
 }
 
-func (tl TodoList) Render() string{
-	html := ""
-	for i, item := tl.Items {
-		html += item.Text
-	}
-	return html
+func (tl *TodoList) Render() string{
+	var html bytes.Buffer
+	todoList, _ := template.New("TodoList").Parse(templ)
+	todoList.Execute(&html, tl)
+	return html.String()
 }
 
